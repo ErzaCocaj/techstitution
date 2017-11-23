6b93b853be50cb22d3c94e0b8e9fcbfd24cad652
Erza <erzacocaj@hotmail.com>
2017-11-23 10:36:56 +0100

    changes made

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log(5+6);
+console.log("hello world");
+function consoleName(name)
+{
+	console.log("Emri im eshte",name);
+		
+}
+consoleName();
+
+function consoleSum( number1,number2)
+{
+	console.log(number1+number2);
+	
+}
+consoleSum(2,6);
