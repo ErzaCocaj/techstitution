73f393069f2d488f7c20c26103c8db7e4a648ec1
Erza <erzacocaj@hotmail.com>
2017-11-24 14:32:39 +0100

    changes made

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-	
+	var datatimeNow= new Date();
+	 console.log("DOM is ready",datatimeNow);
 	console.log("Dom is ready!");
 $( '#border-wait-form').submit(function(event){
+	 
 	event.preventDefault();
+	var dateTimeNow=$('#datetime-now').val();
 	var pikaKufitare= $('#pika-kufitare').val();
 	//hyrje-dalje ne minuta
 	var hyrjeMinMinuta=$('#hyrje-min-minuta').val();
@@ -15,12 +18,23 @@ $( '#border-wait-form').submit(function(event){
 	var daljeMinMetra=$('#dalje-min-metra').val();
 	var daljeMaxMetra=$('#dalje-max-metra').val();
 	
+	var dataJson={};
+	dataJson ["pikakufitare"]=pikaKufitare;
+	dataJson ["hyrjeMinMinuta"]= parseInt(hyrjeMinMinuta);
+	dataJson ["hyrjeMaxMinuta"]=parseInt(hyrjeMaxMinuta);
+	dataJson["daljeMinMinuta"]=parseInt(daljeMinMinuta);
+	dataJson["daljeMaxMinuta"]=parseInt(daljeMaxMinuta);
+	dataJson["hyrjeMinMetra"]= parseInt(hyrjeMinMetra);
+	dataJson["hyrjeMaxMetra"]=parseInt(hyrjeMaxMetra);
+	dataJson["daljeMinMetra"]=parseInt(daljeMinMetra);
+	dataJson["daljeMaxMetra"]=parseInt(daljeMaxMetra);
+	dataJson["datatimeNow"]=new Date();
+	 console.log(dataJson);
 	
-	
-	console.log("Pika kufitare: ",pikaKufitare,"Hyrje min: ",hyrjeMinMinuta, "Hyrje max :" ,hyrjeMaxMinuta,"Dalje min:",daljeMinMinuta,
+/*	console.log("Pika kufitare: ",pikaKufitare,"Hyrje min: ",hyrjeMinMinuta, "Hyrje max :" ,hyrjeMaxMinuta,"Dalje min:",daljeMinMinuta,
 	"Dalje max", daljeMaxMinuta);
 	console.log("Hyrje min: ",hyrjeMinMetra, "Hyrje max :", hyrjeMaxMetra,"Dalje min:",daljeMinMetra,
 	"Dalje max", daljeMaxMetra);
-	
+	*/
 });	
 });
