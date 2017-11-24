13e5498865a2176811e9d46e3b33344dde80f359
Erza <erzacocaj@hotmail.com>
2017-11-24 13:36:14 +0100

    changed form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-console.log(5+6);
-console.log("hello world");
-function consoleName(name)
-{
-	console.log("Emri im eshte",name);
-		
-}
-consoleName();
-
-function consoleSum( number1,number2)
-{
-	console.log(number1+number2);
+$(document).ready(function(){
 	
-}
-consoleSum(2,6);
+	console.log("Dom is ready!");
+$( '#border-wait-form').submit(function(event){
+	event.preventDefault();
+	var pikaKufitare= $('#pika-kufitare').val();
+	//hyrje-dalje ne minuta
+	var hyrjeMinMinuta=$('#hyrje-min-minuta').val();
+	var hyrjeMaxMinuta=$('#hyrje-max-minuta').val();
+	var daljeMinMinuta=$('#dalje-min-minuta').val();
+	var daljeMaxMinuta=$('#dalje-max-minuta').val();
+	//hyrje-dalje ne metra
+	var hyrjeMinMetra=$('#hyrje-min-metra').val();
+	var hyrjeMaxMetra=$('#hyrje-max-metra').val();
+	var daljeMinMetra=$('#dalje-min-metra').val();
+	var daljeMaxMetra=$('#dalje-max-metra').val();
+	
+	
+	
+	console.log("Pika kufitare: ",pikaKufitare,"Hyrje min: ",hyrjeMinMinuta, "Hyrje max :" ,hyrjeMaxMinuta,"Dalje min:",daljeMinMinuta,
+	"Dalje max", daljeMaxMinuta);
+	console.log("Hyrje min: ",hyrjeMinMetra, "Hyrje max :", hyrjeMaxMetra,"Dalje min:",daljeMinMetra,
+	"Dalje max", daljeMaxMetra);
+	
+});	
+});
